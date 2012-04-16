28b2b677f538a56bfd9ed844e79d2e04b8691cc1
medar <mickael23@gmail.com>
2012-04-16 23:54:12 +0400

    панель клика по юниту

---
+++ js/engine.js
@@ -0,0 +1,15 @@
+//
+$(function(){
+
+});
+
+function click_unit()
+{
+	$(".sidebar_panel").addClass("hidden");
+	$("#sp_unit").removeClass("hidden");
+}
+function click_city()
+{
+	$(".sidebar_panel").addClass("hidden");
+	$("#sp_city").removeClass("hidden");
+}
